f682c515da4768ad29a39cf9d0e7c4fc8b805f6f
mantyshah <mantyshah31@gmail.com>
2018-07-02 16:41:29 +0530

    Created Custom Info Window for markers.

---
+++ app/src/main/java/com/nearbyplaces/devsoftware/nearbyplaces/CustomeInfoWIndow.java
@@ -0,0 +1,44 @@
+package com.nearbyplaces.devsoftware.nearbyplaces;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class CustomeInfoWIndow implements GoogleMap.InfoWindowAdapter {
+
+
+    private LayoutInflater layoutInflater;
+    private View view;
+    private Context context;
+
+    public CustomeInfoWIndow(Context context) {
+        this.context = context;
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = layoutInflater.inflate(R.layout.custom_info_window, null);
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        TextView placeName, vincity;
+
+        placeName = view.findViewById(R.id.placeName);
+        placeName.setText(marker.getTitle());
+
+        vincity = view.findViewById(R.id.vincity);
+        vincity.setText(marker.getSnippet());
+
+
+
+        return view;
+    }
+}
